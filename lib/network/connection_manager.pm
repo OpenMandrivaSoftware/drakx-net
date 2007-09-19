b8baf7bf894b7fed6072dbbf4ae9951cd4df10f5
Olivier Blin <oblin@mandriva.com>
2007-09-19 15:54:52 +0000

    do not die when no wireless interface is configured (#33123)

--- lib/network/connection_manager.pm
+++ lib/network/connection_manager.pm
@@ -269,8 +269,9 @@ sub update_on_status_change {
     $cmanager->{gui}{buttons}{connect_stop}->set_sensitive($cmanager->{connection} && $cmanager->{connection}->get_status)
       if $cmanager->{gui}{buttons}{connect_stop};
 
-    my $may_have_network = !$cmanager->{connection}->can('get_networks') || $cmanager->{connection}{network};
-    $cmanager->{gui}{buttons}{configure}->set_sensitive($cmanager->{connection} && $may_have_network)
+    my $may_have_network = $cmanager->{connection} &&
+      (!$cmanager->{connection}->can('get_networks') || $cmanager->{connection}{network});
+    $cmanager->{gui}{buttons}{configure}->set_sensitive($may_have_network)
       if $cmanager->{gui}{buttons}{configure};
 }
 
