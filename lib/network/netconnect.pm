01e3026123d4c32438e0292993192c6b0b91c5ba
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-02-25 18:35:29 +0000

    be stricter when matching provider country
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-net/trunk@237396 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/network/netconnect.pm
+++ lib/network/netconnect.pm
@@ -189,7 +189,8 @@ sub real_main {
                            @providers_data = $connection->get_providers;
                            require lang;
                            my $locale_country = lang::c2name($::o->{locale}{country} || lang::read()->{country});
-                           $provider_name = find { /^\Q$locale_country\E/ } sort(keys %{$providers_data[0]});
+                           my $separator = $providers_data[1];
+                           $provider_name = find { /^\Q$locale_country$separator\E/ } sort(keys %{$providers_data[0]});
                        },
                        name => sub { $net->{type}->get_type_name . "\n\n" . N("Please select your provider:") },
                        data => sub {
