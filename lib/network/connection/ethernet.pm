18bb8051450f2c1052bcd00ec22466c475bc644f
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-04-02 23:05:45 +0000

    add a link detection delay for b44 module (#38795)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-net/trunk@241795 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/network/connection/ethernet.pm
+++ lib/network/connection/ethernet.pm
@@ -519,7 +519,7 @@ sub configure_eth_aliases {
 
 sub get_link_detection_delay {
     my ($self) = @_;
-    member($self->get_driver, qw(forcedeth r8169 skge sky2 tg3)) && 6;
+    member($self->get_driver, qw(b44 forcedeth r8169 skge sky2 tg3)) && 6;
 }
 
 sub is_ifplugd_blacklisted {
