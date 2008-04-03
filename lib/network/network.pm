c1150f908afb9fa176dbbf3987de9a17c11ae557
Olivier Blin <oblin@mandriva.com>
2008-04-03 04:02:15 +0000

    do not write MII_NOT_SUPPORTED in wireless.d files (so that its value does not get overwritten when switching networks)

--- lib/network/network.pm
+++ lib/network/network.pm
@@ -230,7 +230,7 @@ sub write_wireless_conf {
     my %wireless_ifcfg = %$ifcfg;
     # FIXME: be smarter to keep only DHCP/IP settings here
     delete $wireless_ifcfg{$_}
-      foreach qw(DEVICE);
+      foreach qw(DEVICE MII_NOT_SUPPORTED);
     write_interface_settings(\%wireless_ifcfg, $wireless_file);
 }
 
