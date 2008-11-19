fc5b88010e2084ae01b7df2b3a43c2e60e9ddc2b
Olivier Blin <oblin@mandriva.com>
2008-11-19 17:20:27 +0000

    fix installing rt61 and rt73 firmware packages

--- lib/network/connection/wireless.pm
+++ lib/network/connection/wireless.pm
@@ -264,7 +264,8 @@ my @thirdparty_settings = (
     },
 
     {
-        name => 'rt61pci',
+        name => 'rt61',
+        matching => 'rt61pci',
         description => 'Ralink RT61 802.11abg WLAN',
         firmware => {
             url => 'http://rt2x00.serialmonkey.com/',
@@ -273,7 +274,8 @@ my @thirdparty_settings = (
     },
 
     {
-        name => 'rt73usb',
+        name => 'rt73',
+        matching => 'rt73usb',
         description => 'Ralink RT73 802.11abg WLAN',
         firmware => {
             url => 'http://rt2x00.serialmonkey.com/',
