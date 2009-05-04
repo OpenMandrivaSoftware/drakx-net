4fc88a98de5b52f8569c11c502bc23ec2216ce17
eugeni <eugeni@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-05-04 13:41:41 +0000

    Properly detecting Sierra Wireless cards (#48849).
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-net/trunk@257186 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/network/connection/cellular_card.pm
+++ lib/network/connection/cellular_card.pm
@@ -15,7 +15,8 @@ sub get_devices() {
        my @serial = grep { $_->{description} =~ /GPRS|EDGE|3G|UMTS|H.DPA|CDMA/i } detect_devices::matching_driver('serial_cs', 'usbserial', @maybe_usbserial_modules);
        member($_->{driver}, @maybe_usbserial_modules) and $_->{driver} = 'usbserial' foreach @serial;
        #- cdc_acm can not be listed directly in network/cellular, it is already in network/isdn
-       @serial, detect_devices::probe_category('network/cellular'), detect_devices::matching_driver('cdc_acm');
+       #- sierra wireless is also not detected as a 3G card
+       @serial, detect_devices::probe_category('network/cellular'), detect_devices::matching_driver('cdc_acm', 'sierra');
 }
 sub get_metric { 40 }
 
