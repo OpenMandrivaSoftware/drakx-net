OpenMandrivaSoftware/drakx-net

commit 0fc9cd671512c0a14300d2c0c509263b4393bbe4

remove old Ifw rule if needed
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-net/trunk@239468 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

lib/network/drakfirewall.pm
@@ -240,6 +240,9 @@ sub set_ifw {
             $_ .= join("\n", @list) . "\n" if eof && $enabled;
         } "$::prefix/etc/shorewall/$file";
     };
+    substInFile {
+            undef $_ if $_ eq "INCLUDE /etc/ifw/rules", "iptables -I INPUT 2 -j Ifw";
+    } "$::prefix/etc/shorewall/start";
     $set_in_file->('start', "INCLUDE /etc/ifw/start", "INCLUDE /etc/ifw/rules", "iptables -I INPUT 1 -j Ifw");
     $set_in_file->('stop', "iptables -D INPUT -j Ifw", "INCLUDE /etc/ifw/stop");
 }

commit bd13735c925a43977f1a7b2f18164e3dc16f8775

(set_ifw) fix cleaning old rules
issue introduced by blino in r239468 on Mar 14 2008:
"remove old Ifw rule if needed"

lib/network/drakfirewall.pm
@@ -291,7 +291,7 @@ sub set_ifw {
     }
 
     substInFile {
-            undef $_ if $_ eq "INCLUDE /etc/ifw/rules", "iptables -I INPUT 2 -j Ifw";
+            undef $_ if member($_, "INCLUDE /etc/ifw/rules", "iptables -I INPUT 2 -j Ifw");
     } "$::prefix/etc/shorewall/start";
     network::shorewall::set_in_file('start', $enabled, "INCLUDE /etc/ifw/start", "INCLUDE /etc/ifw/rules", "iptables -I INPUT 1 -j Ifw");
     network::shorewall::set_in_file('stop', $enabled, "iptables -D INPUT -j Ifw", "INCLUDE /etc/ifw/stop");