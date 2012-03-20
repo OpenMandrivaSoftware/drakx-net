OpenMandrivaSoftware/drakx-net

commit d4220a5de0e696f8ccc9f5805745711f1310768b

apply provider settings when provider is set
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-net/trunk@242689 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

lib/network/connection.pm
@@ -172,7 +172,14 @@ sub set_provider {
     if ($self->{provider_name} ne N("Unlisted - edit manually")) {
         my @providers_data = $self->get_providers;
         $self->{provider} = $providers_data[0]{$self->{provider_name}};
+        $self->apply_provider_settings($net);
     }
+}
+
+sub apply_provider_settings {
+    my ($self, $net) = @_;
+    $self->guess_protocol($net) if $self->can('guess_protocol');
+    $self->guess_access_settings if $self->can('guess_access_settings');
 }
 
 #- check that $self->can('get_providers') first

commit 6258d85341d7a65d7ee2702d10fef4ad0d1c1038

(set_provider) add a FIXME issue about an undeclared variable
undeclared variable $net introduced by blino in r242689 on May 23 2008

lib/network/connection.pm
@@ -181,6 +181,7 @@ sub set_provider {
     if ($self->{provider_name} ne N("Unlisted - edit manually")) {
         my @providers_data = $self->get_providers;
         $self->{provider} = $providers_data[0]{$self->{provider_name}};
+        # FIXME: undeclared variable:
         $self->apply_provider_settings($net);
     }
 }