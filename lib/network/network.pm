c889e4125db0f6028eb4e4ae6542f406f8933ca7
Olivier Blin <blino@mageia.org>
2012-09-05 22:09:05 +0000

    remove invalid character in source code that makes drakx-net tools crash

--- lib/network/network.pm
+++ lib/network/network.pm
@@ -802,7 +802,7 @@ sub easy_dhcp {
 sub reload_net_applet() {
     #- make net_applet reload the configuration
     my $pid = chomp_(`pidof -x net_applet`);
-    $pid and eval { kill 1, $pid };
+    $pid and eval { kill 1, $pid };
 }
 
 sub configure_network {
